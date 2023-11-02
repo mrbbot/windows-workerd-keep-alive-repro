373ccb2fdde26e6449719c37b128736fbe828397
Brendan Coll <bcoll@cloudflare.com>
2023-11-02 16:07:29 +0000

    Add reproduction steps

---
+++ README.md
@@ -0,0 +1,4 @@
+# `windows-workerd-keep-alive-repro`
+
+1. `npm install`
+2. Using Node 20, `node index.mjs`
--- index.mjs
+++ index.mjs
@@ -25,19 +25,23 @@ function spawnWorkerd(configPath, port, env = {}) {
   })
 }
 
+// Start user worker and proxy worker on random ports
 const user1 = await spawnWorkerd("user.capnp", "0", { MESSAGE: "one" });
 const proxy = await spawnWorkerd("proxy.capnp", "0");
 
+// Send request to user worker through proxy worker
 const res1 = await fetch(proxy.url, { headers: { Target: user1.url.href } });
 console.log({ res1: await res1.text() }); // "one"
 
+// Restart user worker on same port
 await user1.kill();
-
 const user2 = await spawnWorkerd("user.capnp", user1.url.port, { MESSAGE: "two" });
 
+// Send request to new user worker through proxy worker
 const res2 = await fetch(proxy.url, { headers: { Target: user1.url.href } });
 console.log({ res2: await res2.text() }); // Internal Server Error, expected "two"
 
+// Send another request to new user worker through proxy worker, now succeeds
 const res3 = await fetch(proxy.url, { headers: { Target: user1.url.href } });
 console.log({ res3: await res3.text() }); // "two"
 
